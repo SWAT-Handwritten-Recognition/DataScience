15be4e89a75b40b4c9026f98b994e80a34011ae2
Jonattan infante vargas <infanpearl@gmail.com>
2020-12-16 09:04:37 -0500

    Se agrega endpoint de verificación

--- AWS/main.py
+++ AWS/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, request, jsonify, render_template,Response
 from load import compare_images
 
 import tensorflow as tf
@@ -22,6 +22,20 @@ def predict():
     frontend_image = request.form['frontend_image']
     result = compare_images(database_image, frontend_image)
     return render_template('index.html', prediction_text='{}'.format(result))
+
+@app.route('/verify', methods=['POST'])
+def up_score():
+    """This function calculates the similarity between two signatures """
+    try:
+        status_code = Response(status=201).status_code
+        info = request.get_json()
+        database_image = info['database_image']
+        frontend_image = info['frontend_image']
+        result = compare_images(database_image, frontend_image)
+        return jsonify({'status':status_code, 'result':result, })
+    except TypeError:
+        print("[Error] ", TypeError.__str__())
+        return jsonify({'Status': 'Please review info body'})
 
 if __name__ == "__main__":
     app.debug=True
